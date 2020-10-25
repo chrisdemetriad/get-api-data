a02d991e28168eeab9a520fc57450a37c01825fc
Chris <chris.demetriad@thecommercepartnership.com>
2020-10-25 04:29:46 +0000

    Getting post titles from /posts

--- App.js
+++ App.js
@@ -1,21 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => {
+	const [data, setData] = useState([]);
+
+	useEffect(() => {
+		getPosts();
+	}, []);
+
+	const renderItem = ({ item }) => {
+		return (
+			<View style={styles.itemRow}>
+				<Text>{item.title}</Text>
+			</View>
+		);
+	};
+
+	const getPosts = async () => {
+		const rawnetApi = "https://cors-anywhere.herokuapp.com/https://rawnet-react-native-test.glitch.me/posts.json";
+		fetch(rawnetApi, {
+			// mode: "no-cors"
+		})
+			.then((response) => response.json())
+			.then((json) => {
+				setData(json);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	};
+
+	return <FlatList style={styles.container} data={data} renderItem={renderItem} keyExtractor={(item, index) => index.toString()} />;
+};
+
+export default App;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#f5fcff",
+	},
+	itemRow: { borderBottomColor: "#eee", padding: 10, borderBottomWidth: 1 },
 });
